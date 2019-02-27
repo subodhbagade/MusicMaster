268fef9bca0b8a6b5df11c427aaab54b96b1bb28
subodh <subodh_bagade07@yahoo.com>
2019-02-27 09:28:59 -0500

    Final Project

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Artist from "./Artist";
+import Tracks from "./Tracks";
+import Search from "./Search";
+
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
+class App extends Component {
+    state = { artist: null, tracks: [] };
+
+    componentDidMount() {
+        this.searchArtist('rehman');
+    }
+
+    searchArtist = artistQuery => {
+        fetch(`${API_ADDRESS}/artist/${artistQuery}`)
+            .then(response => response.json())
+            .then(json => {
+                if(json.artists.total > 0){
+                    const artist = json.artists.items[0];
+
+                    this.setState({ artist });
+
+                    fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+                        .then(response => response.json())
+                        .then(json => this.setState({ tracks: json.tracks }))
+                        .catch(error => alert(error.message));
+                }
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Music Master</h2>
+                <Search searchArtist={this.searchArtist} />
+                <Artist artist={this.state.artist}/>
+                <Tracks tracks={this.state.tracks}/>
+            </div>
+        )
+    }
+}
+
+export default App;
